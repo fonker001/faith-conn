e11de359a4eeae89bfb189db7d619a1961e49a73
Fonker <oganga1aus@gmail.com>
2025-12-11 23:03:32 +0300

    test

---
+++ tailwind.config.cjs
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: 'class',
+  content: [
+    './src/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["var(--font-open-sans)", "sans-serif"],
+        serif: ["var(--font-cormorant)", "serif"],
+      },
+    },
+  },
+  plugins: [],
+};
