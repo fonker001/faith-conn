ebc07ce8030ec25ed13108a80a864b86a97a2f03
Fonker <oganga1aus@gmail.com>
2025-12-13 17:45:47 +0300

    fixes2

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  experimental: {
+    turbopack: false,
+  },
   images: {
     unoptimized: true, // Enable Next.js image optimization
     remotePatterns: [
